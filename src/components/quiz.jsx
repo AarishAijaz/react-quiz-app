9be459fcf4e1fff49610546ab17f52582fe22ba9
Aarish Aijaz <aarishaijaz9@gmail.com>
2026-09-10 15:43:04 +0500

    Initial commit

---
+++ src/components/quiz.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import Results from "./results";
+
+function Quiz(){
+    const questionBank= [
+        {
+            question: "Capital of Pakistan?",
+            options: ["Karachi", "Islamabad", "Lahore"],
+            answer: "Islamabad",
+        },
+        {
+            question: "Language used for web apps?",
+            options: ["PHP", "JavaScript", "All"],
+            answer: "All",
+        },
+        {
+            question: "JSX stands for?",
+            options: ["JavaScript XML", "JavaScript eXtension", "None of the above"],
+            answer: "JavaScript XML",
+        },
+    ];
+
+    const initialAnswers= [null, null, null];
+
+    const[userAnswers, setUserAnswers]= useState(initialAnswers);
+    
+    const[currentQuestion, setCurrentQuestion]= useState(0);
+
+    const[isQuizFinished, setIsQuizFinished]= useState(false);
+
+    const selectedAnswer= userAnswers[currentQuestion];
+
+    function handleSelectOption(option){
+        const newUserAnswers= [...userAnswers];
+        newUserAnswers[currentQuestion]= option;
+
+        setUserAnswers(newUserAnswers);
+    }
+
+    function goToNext(){
+        if(currentQuestion===questionBank.length-1){
+            setIsQuizFinished(true);
+        }
+        else{
+            setCurrentQuestion(currentQuestion+1);
+        }
+    }
+
+    function goToPrev(){
+        if(currentQuestion>0){
+            setCurrentQuestion(currentQuestion-1);
+        }
+    }
+
+    function restartQuiz(){
+        setUserAnswers(initialAnswers);
+        setCurrentQuestion(0);
+        setIsQuizFinished(false);
+    }
+
+    if(isQuizFinished){
+        return <Results 
+                    userAnswers={userAnswers} 
+                    questionBank={questionBank}
+                    restartQuiz={restartQuiz}
+                />;
+    }
+
+    return (
+        <div>
+            <h2>Question {currentQuestion+1}</h2>
+            <p className="question">{questionBank[currentQuestion].question}</p>
+
+            {questionBank[currentQuestion].options.map((option)=> {
+                return <button className={"option" + (selectedAnswer===option ? " selected":"")}
+                onClick={()=> handleSelectOption(option)}>{option}</button>
+            })}
+
+            <div className="nav-buttons">
+                <button onClick={goToPrev} disabled= {currentQuestion== 0}>Previous</button>
+                <button onClick={goToNext} disabled= {!selectedAnswer}>
+                    {currentQuestion===questionBank.length-1? "Finish Quiz": "Next"}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Quiz;
